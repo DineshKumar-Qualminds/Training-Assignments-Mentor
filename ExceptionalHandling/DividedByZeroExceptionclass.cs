e0dcf89a54213257154375795d00d70776f15d0e
Dinesh Kumar Nadakuduru <ndkumar@qualminds.com>
2024-05-29 17:10:57 +0530

    latest commit

---
+++ ExceptionalHandling/DividedByZeroExceptionclass.cs
@@ -0,0 +1,28 @@
+﻿namespace ExceptionalHandling
+{
+    internal class DividedByZeroExceptionclass
+    {
+        static void Main(string[] args)
+        {
+         
+            int x;
+            Console.Write("Enter x value: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            int y;
+            Console.Write("Enter y value: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int z = y / x;
+
+            }catch(DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divided by zero");
+
+            }
+
+
+        }
+    }
+}
