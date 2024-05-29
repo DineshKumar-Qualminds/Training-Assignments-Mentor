5c23af9476a727d0b9241004954af3888eac896f
Dinesh Kumar Nadakuduru <ndkumar@qualminds.com>
2024-05-29 17:15:16 +0530

    latest commit

---
+++ Inheritance/MultiLevel-Inheritance.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class MultiLevel_Inheritance
+    {
+        class Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Animal is eating.");
+            }
+        }
+
+        class Dog : Animal
+        {
+            public void Bark()
+            {
+                Console.WriteLine("Dog is barking.");
+            }
+        }
+
+        class Labrador : Dog
+        {
+            public void Display()
+            {
+                Console.WriteLine("Labrador is a type of dog.");
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                Labrador labrador = new Labrador();
+                labrador.Eat();     // Accessing base class method
+                labrador.Bark();    // Accessing intermediate class method
+                labrador.Display(); // Accessing derived class method
+            }
+        }
+
+    }
+}
---
+++ Inheritance/Multiple-Inheritance .cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Multiple_Inheritance
+    {
+        interface IWalk
+        {
+            void Walk();
+        }
+
+        interface ISwim
+        {
+            void Swim();
+        }
+
+        class Dog : IWalk
+        {
+            public void Walk()
+            {
+                Console.WriteLine("Dog is walking.");
+            }
+        }
+
+        class Fish : ISwim
+        {
+            public void Swim()
+            {
+                Console.WriteLine("Fish is swimming.");
+            }
+        }
+
+        class Tortoise : IWalk, ISwim
+        {
+            public void Walk()
+            {
+                Console.WriteLine(" Tortoise is walking.");
+            }
+
+            public void Swim()
+            {
+                Console.WriteLine(" Tortoise is swimming.");
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                Dog dog = new Dog();
+                dog.Walk(); // Accessing method from IWalk
+
+                Fish fish = new Fish();
+                fish.Swim(); // Accessing method from ISwim
+
+                Tortoise tortoise = new Tortoise();
+                tortoise.Walk(); // Accessing method from IWalk
+                tortoise.Swim(); // Accessing method from ISwim
+            }
+        }
+
+    }
+}
