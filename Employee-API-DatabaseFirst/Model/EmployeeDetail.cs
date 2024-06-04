79f6a9c0f4eb4494d789136ba0ef4088bb55636b
Dinesh Kumar Nadakuduru <ndkumar@qualminds.com>
2024-06-04 11:59:37 +0530

    Database-first changes

---
+++ Employee-API-DatabaseFirst/Model/EmployeeDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Employee_API_DatabaseFirst.Model;
+
+public partial class EmployeeDetail
+{
+    public int EmpId { get; set; }
+
+    public string? EmpName { get; set; }
+
+    public string? DeptName { get; set; }
+
+    public int? Salary { get; set; }
+}
---
+++ Employee-API-DatabaseFirst/Program.cs
@@ -0,0 +1,43 @@
+
+using Employee_API_DatabaseFirst.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_API_DatabaseFirst
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+
+            builder.Services.AddDbContext<EmployeeDatabaseContext>(opt => opt.UseSqlServer
+           (builder.Configuration.GetConnectionString("cn")));
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
